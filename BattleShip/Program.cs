f8c3bece52fea81003938f2c50cf0afe475cab63
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-07 14:03:38 -0700

    Setting up rough draft for different methods and variables

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -6,67 +6,93 @@ namespace BattleShip
     {
         static void Main(string[] args)
         {
-            Console.Title = "FIRE! Find the Enemy";
+            //    Console.Title = "FIRE! Find the Enemy";
 
-            //global variables
-            var fireAttempts =  8;
-            var enemyLife = 5;
+            //    //global variables
+            //    var fireAttempts =  8;
+            //    var enemyLife = 5;
+            //    var outCome;
 
-            //enemy head
-            Random random = new Random();
-            int headY = random.Next(1, 10);
-            int headX = random.Next(1, 5);
+            //    //grid
+            //    string[,] Row = [10, 10];
 
-            //body
-            int bodyY1 = headY;
-            int bodyX1 = headX + 1;
+            //    //enemy head
+            //    Random random = new Random();
+            //    int headY = random.Next(1, 10);
+            //    int headX = random.Next(1, 5);
 
-            int bodyY2 = headY;
-            int bodyX2 = bodyX1 + 1;
+            //    //body
+            //    int bodyY1 = headY;
+            //    int bodyX1 = headX + 1;
 
-            int bodyY3 = headY;
-            int bodyX3 = bodyX2 + 1;
+            //    int bodyY2 = headY;
+            //    int bodyX2 = bodyX1 + 1;
 
-            int bodyY4 = headY;
-            int bodyX4 = bodyX3 + 1;
+            //    int bodyY3 = headY;
+            //    int bodyX3 = bodyX2 + 1;
 
-            
+            //    int bodyY4 = headY;
+            //    int bodyX4 = bodyX3 + 1;
 
-        Restart:
-            Console.Write("Please input a number: ");
-            int numberInputX = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Please input a number: ");
-            int numberInputY = Convert.ToInt32(Console.ReadLine());
 
-            //conditionals hit or miss
-            if(numberInputX == headX && numberInputY == headY)
-            {
-                enemyLife--;
-                Console.WriteLine("hit");
-            } else
-            {
-                Console.WriteLine("miss");
-            }
-            fireAttempts--;
 
-            //conditionals gameover
-            if(fireAttempts == 0)
-            {
-                Console.WriteLine("gameover you lose");
-            } 
+            //Restart:
 
-            if(enemyLife == 0)
-            {
-                Console.WriteLine("gamveover you win");
-            }
-            
 
-            Console.Clear();
-            Console.WriteLine("you choose {0} and {1} as the number", numberInputX, numberInputY);
-            
-            goto Restart;
+            //    //error handling
+            //    try
+            //    {
+            //    Console.Write("Please input a number: ");
+            //    int numberInputX = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Please input a number: ");
+            //    int numberInputY = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
 
-            
+            //    //conditionals hit or miss
+            //    if(numberInputX == headX && numberInputY == headY)
+            //    {
+            //        enemyLife--;
+            //        outCome = "hit";
+            //    } else
+            //    {
+            //        outCome = "miss";
+            //    }
+            //    fireAttempts--;
+
+            //    //conditionals gameover
+            //    if(fireAttempts == 0)
+            //    {
+            //        Console.WriteLine("gameover you lose");
+            //    } 
+
+            //    if(enemyLife == 0)
+            //    {
+            //        Console.WriteLine("gamveover you win");
+            //    }
+
+
+            //    Console.Clear();
+            //    Console.WriteLine("you choose {0} and {1} as the number, it was a {2}", numberInputX, numberInputY, outCome);
+
+            //    goto Restart;
+
+
+            Console.WriteLine("=================================================================");
+            Console.WriteLine("   0 1 2 3 4 5 6 7 8 9 ");
+            Console.WriteLine(" 1 - - - - - - - - - - ");
+            Console.WriteLine(" 2 - - - - - - - - - - ");
+            Console.WriteLine(" 3 - - - - - - - - - - ");
+            Console.WriteLine(" 4 - - - - - - - - - - ");
+            Console.WriteLine(" 5 - - - - - - - - - - ");
+            Console.WriteLine(" 6 - - - - - - - - - - ");
+            Console.WriteLine(" 7 - - - - - - - - - - ");
+            Console.WriteLine(" 8 - - - - - - - - - - ");
+            Console.WriteLine(" 9 - - - - - - - - - - ");
+            Console.WriteLine("=================================================================");
         }
     }
 }
